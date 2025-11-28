475458038974ee8aa657a06401e5fd45165e8133
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-11-28 14:42:03 +0000

    feat(hw0): intro §3.2 - User limits

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -3,8 +3,31 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
-    return 0;
+    int err, errcnt = 0;
+    
+    err = getrlimit(RLIMIT_STACK, &lim);
+    if (err == 0) {
+        printf("stack size: %ld\n", lim.rlim_cur);
+    } else {
+        errcnt <<= 1;
+        errcnt++;
+    }
+
+    err = getrlimit(RLIMIT_NPROC, &lim); // ! Docker does not set a per-user process limit
+    if (err == 0) {
+        printf("process limit: %ld\n", lim.rlim_cur);
+    } else {
+        errcnt <<= 1;
+        errcnt++;
+    }
+
+    err = getrlimit(RLIMIT_NOFILE, &lim);
+    if (err == 0) {
+        printf("max file descriptors: %ld\n", lim.rlim_cur);
+    } else {
+        errcnt <<= 1;
+        errcnt++;
+    }
+
+    return errcnt;
 }
